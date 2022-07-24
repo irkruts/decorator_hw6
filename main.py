b51bc87ad1060e1cc3130532632bc8574e2216ad
Iryna Kruts <i_kruts@ukr.net>
2022-07-24 23:39:43 +0300

    added kwargs

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def decorator(fn):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         print(f'FUNCTION NAME: {fn.__name__} ,with arguments: {args}')
-        fn()
+        return fn(*args, **kwargs)
 
     return wrapper
 
 
 @decorator
-def just_a_function(*args):
-    pass
+def just_a_function(a, b):
+    return a - b + 10
 
 
-just_a_function(4, 5, 6)
+print(just_a_function(4, 5))
